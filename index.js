370a251123a4d07a5b23e231df7513851111c8a0
ddderi <maxime830@live.fr>
2021-12-14 23:36:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks === 43){
+        return 1;
+    } else if (blocks === 50){
+        return 8;
+    } else if (blocks === 34){
+        return 8;
+    }
+}
+
+function distanceFromHqInFeet(feet){
+    // return (feet === 43 || feet === 50) ? 2112 : 264;
+    return feet === 43 ? 264 : 2112;
+}
+
+function distanceTravelledInFeet(feet){
+    return (feet === 43 || feet === 48 ? 1320 :
+    ((feet === 34 || feet === 28) ? 1584 : 2640));
+}
+
+function calculatesFarePrice(start, dest){
+    return start === 43 && dest === 44 ? 0 :
+    (start === 34 && dest === 32 ? 2.56 : (start === 50 && dest === 58 ? 25 : "cannot travel that far"));
+}
+
